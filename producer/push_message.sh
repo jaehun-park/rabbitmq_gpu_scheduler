f9c3071bb6793a623e3d5ce93e19acfa8da77b18
Jaehun Park <jhoon0823@gmail.com>
2023-05-11 00:03:13 +0900

    feat: add rebbitmq base code

---
+++ producer/push_message.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+! python -B producer.py -c ./config_example/exp1.yaml
+! python -B producer.py -c ./config_example/exp2.yaml
+! python -B producer.py -c ./config_example/exp3.yaml
+! python -B producer.py -c ./config_example/exp4.yaml
+! python -B producer.py -c ./config_example/exp5.yaml -p 1
